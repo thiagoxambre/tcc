fccfc89450ee0fcb45c9bee936a2d19ec22a7024
thiagoxambre <thiagoxambre@gmail.com>
2014-11-19 17:30:05 -0200

    database

--- projectmanager/src/main/webapp/database/database.sql
+++ projectmanager/src/main/webapp/database/database.sql
@@ -2,9 +2,9 @@ CREATE DATABASE  IF NOT EXISTS `projectmanager` /*!40100 DEFAULT CHARACTER SET u
 USE `projectmanager`;
 -- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: projectmanager
+-- Host: localhost    Database: projectmanager
 -- ------------------------------------------------------
--- Server version	5.6.21-log
+-- Server version	5.6.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,26 @@ USE `projectmanager`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fornecedor_requisitos`
+--
+
+DROP TABLE IF EXISTS `fornecedor_requisitos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fornecedor_requisitos` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PROJETO_ID` int(11) DEFAULT NULL,
+  `NOME` varchar(100) DEFAULT NULL,
+  `EMAIL` varchar(100) DEFAULT NULL,
+  `TELEFONE` varchar(45) DEFAULT NULL,
+  `OBSERVACAO` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_FORNECEDOR_REQUISITO_PROJETO_idx` (`PROJETO_ID`),
+  CONSTRAINT `FK_FORNECEDOR_REQUISITO_PROJETO` FOREIGN KEY (`PROJETO_ID`) REFERENCES `projeto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `projeto`
@@ -43,7 +63,67 @@ CREATE TABLE `projeto` (
   `telefoneresponsavel` varchar(45) DEFAULT NULL,
   `observacaoresponsavel` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `recurso`
+--
+
+DROP TABLE IF EXISTS `recurso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recurso` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PROJETO_ID` int(11) DEFAULT NULL,
+  `NOME` varchar(100) DEFAULT NULL,
+  `DESCRICAO` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_RECURSO_PROJETO_idx` (`PROJETO_ID`),
+  CONSTRAINT `FK_RECURSO_PROJETO` FOREIGN KEY (`PROJETO_ID`) REFERENCES `projeto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `resumo_usuarios`
+--
+
+DROP TABLE IF EXISTS `resumo_usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resumo_usuarios` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PROJETO_ID` int(11) DEFAULT NULL,
+  `NOME` varchar(100) DEFAULT NULL,
+  `DESCRICAO` varchar(1000) DEFAULT NULL,
+  `RESPONSABILIDADES` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_RESUMO_USUARIOS_PROJETO_idx` (`PROJETO_ID`),
+  CONSTRAINT `FK_RESUMO_USUARIOS_PROJETO` FOREIGN KEY (`PROJETO_ID`) REFERENCES `projeto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `risco`
+--
+
+DROP TABLE IF EXISTS `risco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `risco` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PROJETO_ID` int(11) DEFAULT NULL,
+  `NOME` varchar(100) DEFAULT NULL,
+  `DESCRICAO` varchar(1000) DEFAULT NULL,
+  `TIPO_RISCO` varchar(45) DEFAULT NULL,
+  `CATEGORIA_RISCO` varchar(45) DEFAULT NULL,
+  `PLANO_CONTINGENCIA` varchar(1000) DEFAULT NULL,
+  `PROBABILIDADE` double DEFAULT NULL,
+  `IMPACTO` double DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_RISCO_PROJETO_idx` (`PROJETO_ID`),
+  CONSTRAINT `FK_RISCO_PROJETO` FOREIGN KEY (`PROJETO_ID`) REFERENCES `projeto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -55,4 +135,4 @@ CREATE TABLE `projeto` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-11-10  0:00:50
+-- Dump completed on 2014-11-19 17:28:19
